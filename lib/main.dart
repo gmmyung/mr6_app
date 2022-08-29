3a109b47a2e7477584dc8af2c686bc9d4c029b76
Gyungmin Myung <ryan000502@gmail.com>
2022-08-30 00:56:53 +0900

    multiple graphs, caching

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:location_permissions/location_permissions.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,49 +17,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'mr6',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Raspberry Pi BLE Demo'),
+      home: const MyHomePage(title: 'Raspberry Pi BLE Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+
+  final int dataCacheLength = 300;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   bool _foundDeviceWaitingToConnect = false;
   bool _scanStarted = false;
   bool _connected = false;
   String _serviceRead = '';
+  List<FlSpot> _tempData = [];
   late DiscoveredDevice _ubiqueDevice;
+  late StreamSubscription<List<int>> subscribeStream;
   final flutterReactiveBle = FlutterReactiveBle();
   late StreamSubscription<DiscoveredDevice> _scanStream;
   late QualifiedCharacteristic _rxCharacteristic;
@@ -85,7 +71,6 @@ class _MyHomePageState extends State<MyHomePage> {
           flutterReactiveBle.scanForDevices(withServices: []).listen((device) {
         // Change this string to what you defined in Zephyr
         if (device.name == 'Thermometer') {
-          print('Connected!');
           setState(() {
             _ubiqueDevice = device;
             _foundDeviceWaitingToConnect = true;
@@ -113,12 +98,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 serviceId: serviceUuid,
                 characteristicId: characteristicUuid,
                 deviceId: event.deviceId);
-            flutterReactiveBle
+            subscribeStream = flutterReactiveBle
                 .subscribeToCharacteristic(_rxCharacteristic)
                 .listen((data) {
-              print(String.fromCharCodes(data));
               setState(() {
-                _serviceRead = String.fromCharCodes(data);
+                String readString = String.fromCharCodes(data);
+                _serviceRead = readString;
+                double temp = double.parse(readString.split(' ')[0]);
+                _tempData.add(FlSpot(
+                    _tempData.isEmpty ? 0.0 : _tempData.last.x + 1, temp));
+                if (_tempData.length > widget.dataCacheLength * 2) {
+                  _tempData.removeRange(
+                      0, _tempData.length - widget.dataCacheLength);
+                }
               });
             }, onError: (err) {});
             setState(() {
@@ -137,27 +129,24 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _incrementCounter() {
+  void _endConnection() {
+    print("disconnect pressed");
+    subscribeStream.cancel();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _connected = false;
+      _scanStarted = false;
+      _foundDeviceWaitingToConnect = false;
+      _tempData = [];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       persistentFooterButtons: [
+        _connected
+            ? const Text("Connected 👍", style: TextStyle(fontSize: 15))
+            : const Text("Not Connected 🥲", style: TextStyle(fontSize: 15)),
         _scanStarted
             ? ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -175,52 +164,84 @@ class _MyHomePageState extends State<MyHomePage> {
                     primary: Colors.blue, onPrimary: Colors.white),
                 onPressed: _connectToDevice,
                 child: const Icon(Icons.bluetooth))
-            : ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.grey, onPrimary: Colors.white),
-                onPressed: () {},
-                child: const Icon(Icons.bluetooth)),
-        _connected
-            ? ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.blue, onPrimary: Colors.white),
-                onPressed: () {},
-                child: const Icon(Icons.celebration_rounded))
-            : ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.grey, onPrimary: Colors.white),
-                onPressed: () {},
-                child: const Icon(Icons.celebration_rounded)),
+            : _connected
+                ? ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue, onPrimary: Colors.white),
+                    onPressed: _endConnection,
+                    child: const Icon(Icons.stop_rounded))
+                : ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.grey, onPrimary: Colors.white),
+                    onPressed: () {},
+                    child: const Icon(Icons.bluetooth)),
       ],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
-            _connected ? Text(_serviceRead) : const Text('Not Connected'),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Center(
+                  child: _connected
+                      ? Text(_serviceRead)
+                      : const Text('Not Connected')),
+            ),
+            Center(child: RealTimeGraph(data: _tempData, range: 100)),
+            Center(child: RealTimeGraph(data: _tempData, range: 100)),
+            Center(child: RealTimeGraph(data: _tempData, range: 100))
           ],
         ),
       ),
     );
+  }
+}
+
+class RealTimeGraph extends StatefulWidget {
+  const RealTimeGraph({Key? key, required this.data, required this.range})
+      : super(key: key);
+
+  final List<FlSpot> data;
+  final int range;
+
+  @override
+  State<RealTimeGraph> createState() => _RealTimeGraphState();
+}
+
+class _RealTimeGraphState extends State<RealTimeGraph> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.data.isNotEmpty
+        ? Container(
+            padding: const EdgeInsets.all(3),
+            margin: const EdgeInsets.all(3),
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: LineChart(
+              LineChartData(
+                  titlesData: FlTitlesData(
+                    bottomTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    rightTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    topTitles:
+                        AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  ),
+                  maxX: widget.data.last.x,
+                  minX: widget.data.last.x - widget.range,
+                  maxY: 130.0,
+                  minY: 90.0,
+                  clipData: FlClipData.all(),
+                  lineBarsData: [
+                    LineChartBarData(
+                        spots: widget.data,
+                        // spots: widget.data.length < 200
+                        //     ? widget.data
+                        //     : widget.data.sublist(widget.data.length - 200),
+                        dotData: FlDotData(show: false))
+                  ]),
+            ))
+        : const Text("No Data");
   }
 }
